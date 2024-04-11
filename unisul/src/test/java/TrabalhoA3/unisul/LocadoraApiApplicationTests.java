5cb8b3cbff9b67efda38ce331dd6e056719f4906
Jean <100731298+jeankarlo762@users.noreply.github.com>
2024-04-11 18:11:56 -0300

    first commit

---
+++ unisul/src/test/java/TrabalhoA3/unisul/LocadoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package TrabalhoA3.unisul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
